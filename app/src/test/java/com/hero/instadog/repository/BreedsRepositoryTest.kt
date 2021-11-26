9095945c394f2806ed993cf7b6f4650f315a74f8
uelitonfreitas <freitas.ueliton@gmail.com>
2021-11-26 09:38:00 -0300

    test: create repository classes tests

---
+++ app/src/test/java/com/hero/instadog/repository/BreedsRepositoryTest.kt
@@ -0,0 +1,78 @@
+package com.hero.instadog.repository
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.hero.instadog.api.BreedsService
+import com.hero.instadog.database.dao.BreedDao
+import com.hero.instadog.database.model.Breed
+import com.hero.instadog.repository.ApiUtil.successCall
+import com.hero.instadog.repository.model.Resource
+import com.hero.instadog.util.InstantAppExecutors
+import com.hero.instadog.util.mock
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mockito
+
+@RunWith(JUnit4::class)
+class BreedsRepositoryTest {
+
+    private lateinit var breedsRepository: BreedsRepository
+
+    private val breedDao = Mockito.mock(BreedDao::class.java)
+    private val breedService = Mockito.mock(BreedsService::class.java)
+
+    @Rule
+    @JvmField
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun init() {
+        breedsRepository = BreedsRepository(InstantAppExecutors(), breedDao, breedService)
+    }
+
+    @Test
+    fun shouldLoadRepoFromNetworkWithSuccess() {
+        val breedName = "australian"
+        val expectedBreedToBeSaved = Breed(breedName, "https://images.dog.ceo/breeds/airedale/n02096051_1206.jpg")
+        val expectedBreedToBeShow = com.hero.instadog.repository.model.Breed(breedName, "https://images.dog.ceo/breeds/airedale/n02096051_1206.jpg")
+
+        val dbData = MutableLiveData<List<Breed>>()
+        Mockito.`when`(breedDao.load()).thenReturn(dbData)
+
+        val breeds = hashMapOf(breedName to listOf("shepherd"))
+        val call = successCall(
+            com.hero.instadog.api.model.ApiResponseData(
+                message = breeds,
+                status = "success"
+            )
+        )
+        Mockito.`when`(breedService.getBreeds()).thenReturn(call)
+
+        val loadBreedsLiveDataFromRepository = breedsRepository.loadBreeds()
+        Mockito.verify(breedDao).load()
+        Mockito.verifyNoMoreInteractions(breedService)
+
+
+        val observer = mock<Observer<Resource<List<com.hero.instadog.repository.model.Breed>>>>()
+        loadBreedsLiveDataFromRepository.observeForever(observer)
+
+        Mockito.verifyNoMoreInteractions(breedService)
+
+        Mockito.verify(observer).onChanged(Resource.loading(null))
+
+        val updatedDbData = MutableLiveData<List<Breed>>()
+        Mockito.`when`(breedDao.load()).thenReturn(updatedDbData)
+
+        dbData.postValue(emptyList())
+        Mockito.verify(breedService).getBreeds()
+        Mockito.verify(breedDao).insertBreeds(listOf(expectedBreedToBeSaved))
+
+        updatedDbData.postValue(listOf(expectedBreedToBeSaved))
+        Mockito.verify(observer).onChanged(Resource.success(listOf(expectedBreedToBeShow)))
+    }
+
+}
